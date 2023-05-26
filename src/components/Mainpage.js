f03c9bd96f0d9a10c7225d7819e0cf5d38cc6725
hnnav <henna.vahatiitto@gmail.com>
2023-05-26 10:35:16 +0300

    double quote test

--- src/components/Mainpage.js
+++ src/components/Mainpage.js
@@ -4,7 +4,7 @@ import "../styles/MainPage.scss";
 function Mainpage() {
 	return (
 		<div className='mainPage'>
-			<h1>Welcome to the website!</h1>
+			<h1>Welcome to the website!!</h1>
 			<button>Click here</button>
 		</div>
 	);
