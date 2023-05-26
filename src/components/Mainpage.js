fcd4b33169d5046e320d035649f6e420f334a42b
hnnav <henna.vahatiitto@gmail.com>
2023-05-26 09:46:57 +0300

    double quote

--- src/components/Mainpage.js
+++ src/components/Mainpage.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import '../styles/MainPage.scss'
+import React from "react";
+import "../styles/MainPage.scss";
 
 function Mainpage() {
-  return (
-    <div className='mainPage'>
-        <h1>Welcome to the website</h1>
-        <button>Click here</button>
-    </div>
-  )
+	return (
+		<div className='mainPage'>
+			<h1>Welcome to the website</h1>
+			<button>Click here</button>
+		</div>
+	);
 }
 
-export default Mainpage
+export default Mainpage;
